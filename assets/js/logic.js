45e273f6f9f3382923e43d3cd7e5c6481e3a35ef
Aizeke <iguido97@icloud.com>
2019-11-14 15:31:29 -0800

    Added changes to the format, added scrolling animation

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -18,18 +18,57 @@
 
             const cover = $('#map-cover');
 
-            if ( cover.data("map-active") === "on" ){
+            if (cover.data("map-active") === "on") {
                 cover.addClass("active-map");
                 $(".map-text-on").addClass("display-off");
                 $(".map-text-off").removeClass("display-off");
                 cover.data("map-active", "off");
-            } else { 
+            } else {
                 cover.removeClass("active-map");
                 $(".map-text-off").addClass("display-off");
                 $(".map-text-on").removeClass("display-off");
                 cover.data("map-active", "on");
             }
         })
+    });
+
+
+    // MAP BOX
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWl6ZWtlIiwiYSI6ImNrMnAyNWIyZTAxMHczY3A0azFucHJyMzEifQ.eaBU6RYrSCwd079_pGSV6A';
+
+    var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+        center: [-117.82, 33.68], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+    });
+
+    var geojson = {
+        type: 'FeatureCollection',
+        features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [-117.82, 33.68]
+            },
+            properties: {
+                title: 'Job Radius',
+                description: 'Orange County, CA'
+            }
+        }]
+    };
+
+    geojson.features.forEach(function (marker) {
+
+        // create a HTML element for each feature
+        var el = document.createElement('div');
+        el.className = 'marker';
+
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(map);
     });
 
 })(jQuery);
